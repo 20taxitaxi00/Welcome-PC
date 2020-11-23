69c514aa211e3aaeb1fa0e7d10b5bed4d9e781a0
20taxitaxi00 <takushi0210@icloud.com>
2020-11-23 19:47:57 +0900

    ドロワー完了

--- JS/app.js
+++ JS/app.js
@@ -19,11 +19,38 @@ $(function(){
 	    $('.js-drower').toggleClass('on');
 	  }
 
+
 	// モーダル
 	// 新規登録、ログインでモーダル
-	
+	$('.js-register, .js-login').on('click', function(){
+		let isRegister = $(this).hasClass('js-register');
+		showModal(isRegister);
+	});
 
+	// モーダルを閉じる
+	$('.js-modal-close').on('click', function(){
+		closeModal();
+	});
 
+	// モーダル関数
+	function showModal(isRegister) {
+		let btn = $('#js-modal-form-btn');
+		if (isRegister) {
+			btn.text('登録');
+			btn.removeClass('login').addClass('register');
+		} else {
+			btn.text('ログイン');
+			btn.removeClass('register').addClass('login');	
+		}
+
+		$('#modal-bg').fadeIn(600);
+		$('.modal-content').fadeIn(800);
+	}
+
+	function closeModal() {
+		$('#modal-bg').fadeOut(800);
+		$('.modal-content').fadeOut(600);
+	}
 
 
 });
